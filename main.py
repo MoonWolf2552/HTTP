8fb85318b2cdc566219bb68bb655c32aeeae2a2e
Smart developer <developer@yandex.ru>
2023-02-15 19:05:02 +0300

    sqlalchemy

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from flask import Flask, url_for, render_template
 import json
 
+from flask import Flask, url_for, render_template, redirect
+
+from forms.loginform import LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
@@ -11,6 +16,14 @@ def index():
     param['username'] = "Ученик Яндекс.Лицея"
     param['title'] = 'Домашняя страница'
     return render_template('index.html', **param)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 @app.route('/odd_even')
