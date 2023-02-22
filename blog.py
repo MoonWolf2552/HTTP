596d18ede0a5cbcdbed4c8a30d96baa797f47cab
Smart developer <developer@yandex.ru>
2023-02-22 18:24:52 +0300

    sqlalchemy3

--- blog.py
+++ blog.py
@@ -1,19 +1,61 @@
 import datetime
 
-from flask import Flask
+from flask import Flask, render_template, redirect
 from data import db_session
 from data.news import News
 from data.users import User
+from forms.loginform import LoginForm
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/index')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
 def main():
     db_session.global_init("db/blogs.db")
-    db_sess = db_session.create_session()
-    # app.run(port=8080, host='127.0.0.1')
+    app.run(port=8080, host='127.0.0.1')
 
+    # db_sess = db_session.create_session()
     # user = User()
     # user.name = "Пользователь 3"
     # user.about = "биография пользователя 3"
@@ -44,11 +86,11 @@ def main():
     # db_sess.add(news)
     # db_sess.commit()
 
-    user = db_sess.query(User).filter(User.id == 1).first()
-    news = News(title="Личная запись", content="Эта запись личная",
-                is_private=True)
-    user.news.append(news)
-    db_sess.commit()
+    # user = db_sess.query(User).filter(User.id == 1).first()
+    # news = News(title="Личная запись", content="Эта запись личная",
+    #             is_private=True)
+    # user.news.append(news)
+    # db_sess.commit()
 
 
 if __name__ == '__main__':
